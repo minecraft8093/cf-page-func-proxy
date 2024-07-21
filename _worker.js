870fe79f9d5c6ae6017f7744e4c22928936987c0
minecraft8093 <62686529+minecraft8093@users.noreply.github.com>
2024-07-22 02:15:29 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,8 +4,10 @@ export default {
         if (url.pathname.startsWith('/')) {
             // 自定义主机名
             url.hostname = "cc.444435.xyz";
-            // 添加自定义端口, 例如 8080
-            url.port = "44300"; // 替换为所需的端口号
+            // 添加自定义端口，确保使用 HTTPS
+            url.protocol = "https"; // 确保使用 HTTPS
+            url.port = "44300"; // 如果你有其他 HTTPS 端口，也可以替换此端口
+
             // 创建新的请求并转发
             let new_request = new Request(url, request);
             return fetch(new_request);
