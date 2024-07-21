83139b7b658bfc5bb392091a5bf61484a8cb39e1
minecraft8093 <62686529+minecraft8093@users.noreply.github.com>
2024-07-22 02:47:07 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,18 +1,16 @@
-export default {
-    async fetch(request, env) {
-        let url = new URL(request.url);
-        if (url.pathname.startsWith('/')) {
-            // 自定义主机名
-            url.hostname = "cc.444435.xyz";
-            // 添加自定义端口，确保使用 HTTPS
-            url.protocol = "https"; // 确保使用 HTTPS
-            url.port = "44300"; // 如果你有其他 HTTPS 端口，也可以替换此端口
-
-            // 创建新的请求并转发
-            let new_request = new Request(url, request);
-            return fetch(new_request);
-        }
-        // 否则，服务静态资源
-        return env.ASSETS.fetch(request);
-    }
-};
+addEventListener(
+  'fetch',event => {
+     let url=new URL(event.request.url);
+     url.hostname='cc.444435.xyz:44300"';
+     if(url.protocol == 'https:') {
+        url.protocol='http:'
+     }
+     let request=new Request(url,event.request);
+     if(request.headers.has("Origin")) {
+       request.headers.delete("Origin");
+     }
+     event.respondWith(
+          fetch(request)
+    )
+  }
+)
